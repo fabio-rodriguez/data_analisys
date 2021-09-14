cc4c374d4895415b0dc19bcc71a0d8065c10c67d
fragnarxx <fabio25.rodriguez@gmail.com>
2021-09-15 00:45:27 +0200

    maneuvers study

--- maneuvers_study.py
+++ maneuvers_study.py
@@ -1,10 +1,27 @@
+import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sn
+
+from math import *
 
 
-df = pd.read_excel('outputs_concat2.xlsx', index_col=0)
+def transitions_preperching():
+
+    feasible_actions = [(round(radians(-a), 5), f) for a in range(5) for f in range(5)]
+
+    transitions = {}
+    for a in range(5):
+        for f in range(5):
+            values = [round(radians(-a+i), 5) for i in [-1, 0, 1]] + [f+i for i in [-1, 0, 1]]
+            next = set(itertools.combinations(values, 2))
+            action = (round(radians(-a), 5), f)
+            transitions[action] = [m for m in next if m in feasible_actions]
+
+    return transitions
+
+
+df = pd.read_excel('data/outputs_concat2.xlsx', index_col=0)
 
 maneuvers=np.array(df['out_action'])
 id_in_seq=np.array(df['id_in_seq'])
@@ -14,7 +31,6 @@ columns = ["id_in_seq", "out_action"]
 
 dd = {id:{} for id in id_in_seq}
 
-print(dd)
 
 for index, i in enumerate(id_in_seq):
 
@@ -25,17 +41,29 @@ for index, i in enumerate(id_in_seq):
 
 print(dd)
 
+maneuvers = {}
+for key in dd.keys():
+    dman = dd[key]
+    if key == 0:
+        continue
+    for m, times in dman.items():
+        try:
+            maneuvers[m] += times
+        except:
+            maneuvers[m] = times
 
-# 
-# states = np.array([eval(state) for state in states_str])
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+mans, times = zip(*maneuvers.items())
+ax.bar(mans,times)
+ax.set_xticklabels(mans)
+plt.show()
 
-# 
-# corrMatrix = df.corr()
 
-# sn.heatmap(corrMatrix, annot=True)
-# plt.show()
+transitions = transitions_preperching()
+print(len(transitions))
 
-# pca = PCA(n_components=5)
-# pca.fit(df)
+for key in dd.keys():
+    if key == 0:
+        continue
 
-# print(pca.components_) 
